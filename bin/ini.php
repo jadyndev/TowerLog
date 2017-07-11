64c0f08715d6f74060dbd483a97ea23e2bb8610e
DO9THW | Jonas <do9thw@gmail.com>
2017-07-11 13:51:36 +0200

    Update ini.php

--- bin/ini.php
+++ bin/ini.php
@@ -1,22 +1,41 @@
 <?php
+$TowerLogVersion = "0.0.2";
 /***Folgender Code muss angepasst werden:***/
 
-$root_url = "http://192.168.178.34/TowerLog/"; //Die URL unter der das Logbuch erreichbar ist. z.B 127.0.0.1 oder example.com/Towerlog oder hostname/TowerLog
+$root_url = "http://[HOSTNAME / IP]/TowerLog/"; //Die URL unter der das Logbuch erreichbar ist. z.B 127.0.0.1 oder example.com/Towerlog oder hostname/TowerLog
 
 /*Datenbank*/
 $db_host = 'localhost';      //Datenbank Server: Hostname oder IP z.B localhost oder 127.0.0.1
 $db_name = 'towerlog';       //Datenbank die für die Speicherung der Logbücher gedacht ist. Empfohlen ist eine Datenbank mit dem Namen "towerlog"
-$db_user = 'root';         //Datenbank Benutzer. Der Benutzer auf dem SQL Server
-$db_password = ''; //Datenbank Benutzer Passwort. Das Passwort für den Benutzer auf dem SQL Server
+$db_user = '[USER]';         //Datenbank Benutzer. Der Benutzer auf dem SQL Server
+$db_password = '[USER]'; //Datenbank Benutzer Passwort. Das Passwort für den Benutzer auf dem SQL Server
 
+/***Admin***/
+//Passe $admin_zugang so an:
+// "local": Zugang nur über den Rechner auf dem TowerLog läuft.
+// "all":   Zugang für ALLE
+// "none":   Zugang für NIEMANDEN
+$admin_zugang = "local";
+
+/**Updates**/
+//Automatische suche nach Updates.
+//Setze es auf false um die Suche nach Updates auszuschalten
+$UPDATE_enable = true;
 /*******************************************/
 
-//error_reporting(0);
+error_reporting(0);
 session_start();
-$_SESSION['TowerLogVersion'] = "0.0.1";
 
 
 if(!isset($no_sql)){
   $mysql = new mysqli($db_host, $db_user, $db_password, $db_name);
+}
+
+//Suche nach Updates
+if($UPDATE_enable){
+  $TowerLogNewVersion = file_get_contents('https://do9thw.de/TowerLog/UPDATE');
+  if($TowerLogVersion != $TowerLogNewVersion){
+    echo "Update auf Version ".$TowerLogNewVersion." ist verf&#xfc;gbar.";
+  }
 }
 ?>
